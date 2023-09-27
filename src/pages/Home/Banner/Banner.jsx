7ff42143f7eb92f7fc80d55a106394761ec9f84d
md ranit rubbyat sultan <mdranitrubbyatsultan@gmail.com>
2023-09-27 11:46:13 +0600

    search functionality is ready to go

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,17 +1,24 @@
+import PropTypes from 'prop-types';
 import './Banner.css';
+import { useRef } from 'react';
 
-const Banner = () => {
+const Banner = ({ handelSearch }) => {
+    const searchRef = useRef(null);
     return (
         <div className='w-full px-8 py-16 md:py-40 banner-bg'>
             <h2 className='text-5xl text-center font-bold'>I Grow By Helping People In Need</h2>
             <div className='container mx-auto flex items-center justify-center pt-10'>
-                <input className='p-4 border rounded-l-lg w-1/2' type="text" placeholder='Search here....' />
-                <div className='px-7 py-4 bg-[#FF444A] text-white font-semibold rounded-r-lg border border-[#FF444A] hover:cursor-pointer' >
-                    <input className='hover:cursor-pointer' type="button" value="submit" />
+                <input ref={searchRef} className='p-4 border rounded-l-lg w-1/2' type="text" placeholder='Search here....' />
+                <div onClick={() => handelSearch(searchRef.current.value)} className='px-7 py-4 bg-[#FF444A] text-white font-semibold rounded-r-lg border border-[#FF444A] hover:cursor-pointer' >
+                    <input className='hover:cursor-pointer' type="button" value="Search" />
                 </div>
             </div>
         </div>
     );
 };
+
+Banner.propTypes = {
+    handelSearch: PropTypes.func,
+}
 
 export default Banner;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,19 +1,40 @@
 import { useLoaderData } from 'react-router-dom';
 import Banner from '../Banner/Banner';
 import DonationCard from '../DonationCard/DonationCard';
+import { useState } from 'react';
 
 const Home = () => {
-
     const donationCardData = useLoaderData();
+    const [searchCategory, setSearchCategory] = useState(() => donationCardData);
+    const [noDataFound, setNoDataFond] = useState(() => false);
+
+    const handelSearch = search => {
+        setNoDataFond(() => false);
+        if (search === '') {
+            setSearchCategory(() => donationCardData);
+        } else {
+            const searchedCategory = donationCardData.filter(category => category?.Category.toLowerCase().includes(search.toLowerCase()));
+
+            if (searchedCategory.length > 0) {
+                setSearchCategory(() => searchedCategory);
+            } else {
+                setSearchCategory(() => searchedCategory);
+                setNoDataFond(() => true);
+            }
+        }
+    }
 
     return (
         <div>
-            <Banner />
+            <Banner handelSearch={handelSearch} />
             <div className='container mx-auto my-12 px-4 lg:px-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6'>
                 {
-                    donationCardData.map((cardData, idx) => <DonationCard key={idx} cardData={cardData} />)
+                    searchCategory.map((cardData, idx) => <DonationCard key={idx} cardData={cardData} />)
                 }
             </div>
+            {
+                noDataFound && <p className='text-center text-4xl text-red-500 font-bold'>No matched data found!</p>
+            }
         </div>
     );
 };
